421aa4b35e8e72a244ad1319ec789dcec71fd9b6
unknown <brhouma85@gmail.com>
2023-04-22 13:02:47 -0700

    working on script

--- Assets/script.js
+++ Assets/script.js
@@ -3,18 +3,18 @@ let contentEl = document.querySelector(".content");
 let startBtn = document.getElementById("start-button");
 let quizStart = document.getElementById('quiz-start');
 let timeEl = document.getElementById('time-id');
-timeEl.textContent = timeOnClock; 
+timeEl.textContent = timeOnClock;
 let quizHomeEl = document.querySelector('.quiz-home');
 let startPageHeader = document.getElementById('start-page-header');
 let answerResult = document.getElementById('answer-result');
-let answerBtn ;
+let answerBtn;
 let currentQuestion;
 let currentAnswers;
 let xQuestion = 0;
 let timeOnClock;
 timeOnClock = 60
 let timeInt = "";
-let score = 0; 
+let score = 0;
 quizEnd.remove();
 userScoreEl.remove();
 let quizHome = document.createElement('button');
@@ -78,27 +78,57 @@ let questionAnswerObjArr = [
     },
 ]
 
-function startQuiz(){
-    timeInt = setInterval(startClock,1000);
+function startQuiz() {
+    timeInt = setInterval(startClock, 1000);
 }
 
 function startClock() {
-    
+
     timeOnClock--;
     timeEl.textContent = timeOnClock;
-    
-    if(timeOnClock <= 0) {
+
+    if (timeOnClock <= 0) {
         return endQuiz();
     }
 }
 
-function endQuiz(){
-        clearInterval(timeInt);
-        currentAnswers.remove();
-        currentQuestion.remove();
-    
-        contentEl.append(quizEnd);
-       
-        let submitScore = document.querySelector(".submit-button");
-        submitScore.addEventListener('click', saveScore);
+function endQuiz() {
+    clearInterval(timeInt);
+    currentAnswers.remove();
+    currentQuestion.remove();
+
+    contentEl.append(quizEnd);
+
+    let submitScore = document.querySelector(".submit-button");
+    submitScore.addEventListener('click', saveScore);
+}
+
+function moveToQuestions() {
+    startBtn.remove();
+    quizStart.remove();
+    startPageHeader.remove();
+
+    var questionContainerEl = document.getElementById("question-container-id");
+    var listOfAnswers = document.createElement('ol');
+    var questionText = document.createElement('p');
+
+    if (xQuestion === questionAnswerObjArr.length) {
+        questionAnswerObjArr.answers = ["finished"];
+        return endQuiz();
+    }
+
+    for (i = 0; i < questionAnswerObjArr[xQuestion].answers.length; i++) {
+
+        answerBtn = document.createElement('button');
+        answerBtn.textContent = questionAnswerObjArr[xQuestion].answers[i];
+        answerBtn.addEventListener('click', handleSelectedAnswer);
+
+        questionText.textContent = questionAnswerObjArr[xQuestion].question;
+        currentQuestion = questionText;
+        currentAnswers = listOfAnswers;
+
+        listOfAnswers.append(answerBtn);
+        questionContainerEl.append(questionText);
+        questionContainerEl.append(listOfAnswers);
+    }
 }
